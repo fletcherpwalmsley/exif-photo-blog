aa10ffdf4ca0c50b772f2be690e10b187bab1e35
Fletcher Walmsley <fletcherpw99@gmail.com>
2025-01-06 20:21:52 +1300

    Add image to contact page
    Fiddle with the layout a bit more
    Still don't like it, but it will do for now

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,7 @@
 import Container from '@/components/Container';
 import SiteGrid from '@/components/SiteGrid';
+import ImageMedium from '@/components/image/ImageMedium';
+import { clsx } from 'clsx/lite';
 
 export default async function AdminConfigurationPage() {
   return (
@@ -11,20 +13,28 @@ export default async function AdminConfigurationPage() {
               Contact
             </div>
           </div>
-        <Container spaceChildren={false}>
-          <div className="max-w-xl w-full">
-            <div className="space-y-6">
-              <p>
-              Hello! My name is Fletcher Walmsley! - Welcome to my photo blog! <br/> <br/>
-              This is a place where I add photos I think are cool. <br/>
-              Some of the photos I upload are good, others I just like, and a bunch are film photos which I need to post somewhere to justify paying ~$1.4 a photo! <br/>
-              The decent pics are usually taken on my Sony A6400 and can be found under the /shot-on/sony/ilce-6400 tag <br/>
-              My film photos can be found under the /tag/film tag. Each of them is also tagged with the film stock used. E.g. /tag/fujifilm-200
-              </p>
+          <Container spaceChildren={false} centered={true} color={'blue'}>
+            <div className="max-w-xl w-full">
+              <div className="space-y-6">
+                <ImageMedium
+                  src="https://photo-blog-store.fletcherwalmsley.com/photo-CQkwS6FzSfFMP1XH.jpg"
+                  alt="https://photo-blog-store.fletcherwalmsley.com/photo-CQkwS6FzSfFMP1XH.jpg"
+                  aspectRatio={3.0 / 2.0}
+                  className={clsx(
+                    'rounded-[3px] overflow-hidden',
+                  )}
+                />
+                <p>
+                Hello! My name is Fletcher Walmsley! - Welcome to my photo blog! <br/> <br/>
+                This is a place where I add photos I think are cool. <br/>
+                Some of the photos I upload are good, others I just like, and a bunch are film photos which I need to post somewhere to justify paying ~$1.4 a photo! <br/>
+                The decent pics are usually taken on my Sony A6400 and can be found under the /shot-on/sony/ilce-6400 tag <br/>
+                My film photos can be found under the /tag/film tag. Each of them is also tagged with the film stock used. E.g. /tag/fujifilm-200
+                </p>
+              </div>
             </div>
-          </div>
-          </Container>
-          <Container>
+            </Container>
+          <Container color={'blue'}>
           <div className="max-w-xl w-full">
             <div className="space-y-6">
               <p>
