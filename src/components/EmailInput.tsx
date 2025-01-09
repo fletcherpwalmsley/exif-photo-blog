d69939a23aaca53a7da8ff8119cfb1d06bf78060
Fletcher Walmsley <fletcher.walmsley@ptxtrimble.com>
2025-01-09 14:36:58 +1300

    Fix lint warnings in recently added files

--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { useActionState } from "react";
+import { useActionState } from 'react';
 // import { useAppState } from '@/state/AppState';
-import { SubmitEmail } from "@/lib/SubmitEmail";
-import { toast } from "sonner";
-import * as React from "react";
+import { SubmitEmail } from '@/lib/SubmitEmail';
+import { toast } from 'sonner';
+import * as React from 'react';
 
 const EmailInput = () => {
   const [state, setEmail] = useActionState(SubmitEmail, undefined);
@@ -15,9 +15,9 @@ const EmailInput = () => {
       return;
     }
 
-    if ("data" in state) {
+    if ('data' in state) {
       toast(state.data);
-    } else if ("error" in state) {
+    } else if ('error' in state) {
       toast(`Error when sending email: ${state.error}`);
     }
   }, [state]);
@@ -30,12 +30,12 @@ const EmailInput = () => {
     <div>
       <form action={setEmail} style={{ display: 'flex', alignItems: 'center' }}>
         <input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="Enter your email"
+          id='email'
+          name='email'
+          type='email'
+          placeholder='Enter your email'
           required
-          autoComplete="email"
+          autoComplete='email'
           style={{ padding: '10px', marginRight: '10px', flex: 1 }}
         />
         <button type="submit" style={{ padding: '10px 20px' }}>
